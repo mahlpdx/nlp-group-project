0fe093e1d33fb567a70d034ec8b14f064ba9f8af
jingjingzhao1 <jingjing.zhao001@gmail.com>
2022-11-27 17:59:45 -0800

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,4 @@
-import rouge_metric
+from rouge_metric import PyRouge
 """
 Functions to evaluate generated and reference
 summaries with ROUGE metrics. 
@@ -8,10 +8,31 @@ for each rouge metric.
 
 Using this library: https://pypi.org/project/rouge-metric/
 """
+"""
+hypotheses = [
+    'how are you\ni am fine',  # document 1: hypothesis
+    'it is fine today\nwe won the football game',  # document 2: hypothesis
+]
+references = [[
+    'how do you do\nfine thanks',  # document 1: reference 1
+    'how old are you\ni am three',  # document 1: reference 2
+], [
+    'it is sunny today\nlet us go for a walk',  # document 2: reference 1
+    'it is a terrible day\nwe lost the game',  # document 2: reference 2
+]]
+"""
+
+def evaluation(hypotheses, references):
+    # Evaluate document-wise ROUGE scores
+    rouge = PyRouge(rouge_n=(1, 2, 4), rouge_l=True, rouge_w=True,
+                rouge_w_weight=1.2, rouge_s=True, rouge_su=True, skip_gap=4)
+    scores = rouge.evaluate(hypotheses, references)
+    print(scores)
+
 
 if __name__ == '__main__':
     # Run test cases here
-    generated_summary = "My name is jacob and I like water"
-    reference_summary = "My title is jacob and I enjoy drinking"
+    generated_summary = ['My name is jacob and I like water']
+    reference_summary = ['My title is jacob and I enjoy drinking']
 
-    print (1)
+    evaluation(generated_summary,reference_summary)
